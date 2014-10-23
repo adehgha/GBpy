aad3eca0ccfdcea8b53fad47156e364d0963df43
arash_banadaki <a.dehghan.b@gmail.com>
2014-10-23 16:47:36 -0400

    packaging.

---
+++ setup.py
@@ -0,0 +1,16 @@
+mport sys
+import os
+from setuptools import setup
+
+main_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(main_dir, "GBpy"))
+import GBpy
+del sys.path[0]
+
+setup(name='GBpy',
+    version='1.0',
+    description="GBpy is an opensource python package for calculating the geometric properties of interfaces in crsytals.",
+    author='Arash Dehghan Banadaki, Srikanth Patala',
+    author_email='adehgha@ncsu.edu, spatala@ncsu.edu',
+    license='Copyright the authors.'
+)
